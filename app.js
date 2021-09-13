4b9fb678c205070bb67ba96e081bf76ef591f8d4
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-09-13 11:57:02 -0600

    added comparision for loops

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
-
+    // this...
+    for (;et i =0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+    console.log('===========');
+    // Is the same as this...
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 printProfileData(profileDataArgs);
