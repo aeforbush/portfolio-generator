fbf01d153512c3a8f0c50a9d8dc2b7bce50a9dfc
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-09-14 09:20:04 -0600

    refractored code into two files

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 // function that receives input and display data dynamically
 const fs = require('fs');
-const generatePage = (name, github) => {
-return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-</head>
 
-<body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">GitHub</a></h2>
-</body>
-</html>
-` ;
-};
-
-
+// receives exported functions
+const generatePage = require('./src/page-template.js');
 
 // profileDataArgs holds the user command-line arguments (argv holds array)
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
+
 
 const [name, github] = profileDataArgs;
 
-false.writeFile('index.html', generatePage(name, github), err => {
+
+// displays file to browser
+fs.writeFile('index.html', generatePage(name, github), err => {
     if(err) throw err;
     console.log('Portfolio complete!');
 });
