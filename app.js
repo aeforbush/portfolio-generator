9256c4f7b91382450f3268356aa55c20e048e604
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-09-14 11:32:39 -0600

    +function to add multiple projects

--- app.js
+++ app.js
@@ -1,14 +1,83 @@
 // capturing data with Inquirer
 const inquirer = require('inquirer');
-inquirer
-.prompt([
+
+// wrapping object array prompt inside a promptUser function to be invoked on demand
+const promptUser = () => {
+return inquirer.prompt.then(projectData => {
+    portfolioData.projects.push(projectData);
+}
+    [
     {
         type: 'input',
         name: 'name',
         message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
     }
-])
-.then(answers => console.log(answers));
+]);
+};
+
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one (initialized once)
+    if (!portfolioData.projects) {
+    // array to hold multiple projects in the portfolioData object
+    portfolioData.projects = [];
+    };
+    console.log(`
+    ================
+    Add a New Project
+    ================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },    
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }    
+    ]);
+};
+promptUser()
+.then(answers => console.log(answers))
+.then(promptProject)
+.then(projectAnswers => console.log(projectAnswers));
+
+
 // function that receives input and display data dynamically
 // const fs = require('fs');
 
