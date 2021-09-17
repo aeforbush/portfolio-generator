a1c0263ea65218501a9951150cee0e74539c94ce
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-09-16 18:37:27 -0600

    refractored page-template

--- src/page-template.js
+++ src/page-template.js
@@ -16,59 +16,46 @@ return `
 // ${about} generateAbout() ${generateAbout(about)} accepts about variable as a parameter and if it doesn't exist simply turn it into a string
 
 const generateProjects = projectsArr => {
-    // get array of just featured projects
-    const featureProjects = projectsArr.filter(project => {
-        if (project.feature) {
-            return true;
-        }else{
-            return false;
-        }
-    });
-
-    // get array of all non-featured projects
-    const nonFeaturedProjects = projectsArr.filter(project => {
-        if (!project.feature) {
-            return true;
-        }else{
-            return false;
-        }
-    });
-    const featuredProjectHtmlArr = featureProjects.map(({ name, description, languages, link }) => {
-        return `
-        <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-          <h3 class="portfolio-item-title text-light">${name}</h3>
-          <h5 class="portfolio-languages">
-            Built With:
-            ${languages.join(', ')}
-          </h5>
-          <p>${description}</p>
-          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-        </div>
-      `;
-    });
-    const nonFeatureProjectHtmlArr = nonFeaturedProjects.map(({ name, description, languages, link }) => {
-        return `
-        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-          <h3 class="portfolio-item-title text-light">${name}</h3>
-          <h5 class="portfolio-languages">
-            Built With:
-            ${languages.join(', ')}
-          </h5>
-          <p>${description}</p>
-          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-        </div>
-      `;
-    });
     return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-        <!-- Leaving this empty as we'll dynamically insert project HTML here -->
-        ${nonFeatureProjectHtmlArr.join('')}
-        ${featuredProjectHtmlArr.join('')}
+        <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+        .filter(({feature}) => feature)
+    .map(({name, description, languages, link}) => {
+        return `
+        <div class="col-12 mb-2 bg-dark text-light p-3">
+        <h3 class="portfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-langauges">
+        Built With:
+        ${languages.join(', ')}
+        </h5>
+        <p>${description}</p>
+        <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+    `;
+    })
+    .join('')}
+    ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
       </div>
     </section>
   `;
+    
 };
 // export functions to other files
 module.exports = templateData => {
